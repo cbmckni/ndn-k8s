ba20b484d9deb77d1c3e41b82826da517544ca46
Cole McKnight <cbmckni@clemson.edu>
2021-07-11 16:09:16 -0400

    minor fixes

--- genomics/scripts/sync.sh
+++ genomics/scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install kubectl
-apt-get update
+apt-get update -y
 apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg \
 	&& echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list \
